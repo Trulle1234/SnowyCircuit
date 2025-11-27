5f8a791f8a4be8680cc2c0b96377580eb604dbf5
Trulle1234 <leonard.trulsson@gmail.com>
2025-11-27 21:47:53 +0100

    worked on level 3 chaged timer start to first move and fixed the timer

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,6 +3,7 @@ extends Area2D
 @onready var ui: CanvasLayer = $%UI
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@warning_ignore("unused_parameter")
 func _on_body_entered(body: Node2D) -> void:
 	ui.update_coins(1)
 	animation_player.play("pickup")
