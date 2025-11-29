d4186daa3feb7d85c952e201d916f21cdfb99e54
Trulle <leonard.trulsson@gmail.com>
2025-11-30 00:14:15 +0100

    button works (toggeling and doing stuff to be addded

--- scripts/button.gd
+++ scripts/button.gd
@@ -4,27 +4,29 @@ extends Area2D
 
 @onready var collision_shape_on: CollisionShape2D = $StaticBody2D/CollisionShapeOn
 @onready var collision_shape_off: CollisionShape2D = $StaticBody2D/CollisionShapeOff
-@onready var collision_shape_on_body: CollisionShape2D = $CollisionShapeOn
-@onready var collision_shape_off_body: CollisionShape2D = $CollisionShapeOff
 
-var is_on := false
+var is_on: bool = false
+
+var in_range: bool = false 
 
 func _on_body_entered(_body: Node2D) -> void:
-	is_on = !is_on
+	in_range = true
+
+func _on_body_exited(_body: Node2D) -> void:
+	in_range = false
 	
-	if is_on:
-		sprite.play("on")
+func _process(_delta: float) -> void:
+	if in_range and Input.is_action_just_pressed("crouch"):
+		is_on = !is_on
+		
+		if is_on:
+			sprite.play("on")
 
-		collision_shape_on.set_deferred("disabled", false)
-		collision_shape_on_body.set_deferred("disabled", false)
+			collision_shape_on.set_deferred("disabled", false)
+			collision_shape_off.set_deferred("disabled", true)
 
-		collision_shape_off.set_deferred("disabled", true)
-		collision_shape_off_body.set_deferred("disabled", true)
-	else:
-		sprite.play("off")
+		else:
+			sprite.play("off")
 
-		collision_shape_off.set_deferred("disabled", false)
-		collision_shape_off_body.set_deferred("disabled", false)
-
-		collision_shape_on.set_deferred("disabled", true)
-		collision_shape_on_body.set_deferred("disabled", true)
+			collision_shape_on.set_deferred("disabled", true)
+			collision_shape_off.set_deferred("disabled", false)
