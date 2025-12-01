04868e84c60b17385882a128d5dec7ff7defde58
Trulle <leonard.trulsson@gmail.com>
2025-12-01 21:05:09 +0100

    more level

--- scripts/door_2.gd
+++ scripts/door_2.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @onready var animation_player = $AnimationPlayer
 
-func _on_button_2_off() -> void:
+func _on_button_2_on() -> void:
 	animation_player.play("door2_open")
 
-func _on_button_2_on() -> void:
+func _on_button_2_off() -> void:
 	animation_player.play("door2_close")
